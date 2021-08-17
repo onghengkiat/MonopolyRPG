40d1d1918a67f8ec7619b30ebdd4b5cae1c2fe34
Ong Heng Kiat <onghengkiat105@gmail.com>
2021-08-17 12:56:31 +0800

    First commit

---
+++ src/MonopolyRPG/Main.java
@@ -0,0 +1,13 @@
+package MonopolyRPG;
+
+import MonopolyRPG.Controller.GameModePage;
+public class Main  {
+    public static void main(String[] args) {
+
+
+        GameModePage gameModePage=  new GameModePage();
+        gameModePage.start();
+
+    }
+
+}
